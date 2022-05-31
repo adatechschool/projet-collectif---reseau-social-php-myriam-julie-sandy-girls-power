29f9d15b8774d480c6450c97cc2f4010fe7cc25a
Myriam-Mnnr <98812962+Myriam-Mnnr@users.noreply.github.com>
2022-05-31 11:04:06 +0200

    changement des pages feed, tags, subscriptions

--- resoc_n1/Niveau1/news.php
+++ resoc_n1/Niveau1/news.php
@@ -118,7 +118,14 @@
                         </div>
                         <footer>
                             <small>♥ <?php echo $post['like_number'] ?></small>
-                            <a href=""><?php echo $post['taglist'] ?></a>,
+                            <?php 
+                        $arrayTags = explode(',', $post['taglist']);
+                        
+                        foreach($arrayTags as $tags) {
+                        echo '<a href="">#' . $tags . ' </a>';
+                        }
+
+                        ?>
                         </footer>
                     </article>
                     <?php
