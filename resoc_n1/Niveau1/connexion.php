3865b7ea78277b33311a79e2c900b4dd4d2da993
Sandy CS <89575609+Shaa94@users.noreply.github.com>
2022-06-03 14:49:59 +0200

    deconnection et nom OK

--- resoc_n1/Niveau1/connexion.php
+++ resoc_n1/Niveau1/connexion.php
@@ -2,7 +2,6 @@
 
 if(!isset($_SESSION)){
     session_start();
-    echo session_id();
     $userId = intval($_SESSION['connected_id']); 
 }
 
--- resoc_n1/Niveau1/wall.php
+++ resoc_n1/Niveau1/wall.php
@@ -2,63 +2,75 @@
 
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>ReSoC - Mur</title> 
-        <meta name="author" content="Julien Falconnet">
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
-    <?php include('header.php'); ?>
-        <div id="wrapper">
-            <?php
-            /**
-             * Etape 1: Le mur concerne un utilisateur en particulier
-             * La première étape est donc de trouver quel est l'id de l'utilisateur
-             * Celui ci est indiqué en parametre GET de la page sous la forme user_id=...
-             * Documentation : https://www.php.net/manual/fr/reserved.variables.get.php
-             * ... mais en résumé c'est une manière de passer des informations à la page en ajoutant des choses dans l'url
-             */
-            
-            $userId =intval($_GET['user_id']);
-            ?>
-            <?php
-            /**
-             * Etape 2: se connecter à la base de donnée
-             */
-            include('connexionbdd.php'); 
-            ?>
 
-            <aside>
-                <?php
-                /**
-                 * Etape 3: récupérer le nom de l'utilisateur
-                 */  
-                echo $userId;   
-                $laQuestionEnSql = "SELECT * FROM users WHERE id= '$userId' ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                $user = $lesInformations->fetch_assoc();
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
-                //echo "<pre>" . print_r($user, 1) . "</pre>";
-                ?>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
-                <section>
-                    <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : <?php echo $user['alias'];?>
-                        
-                    </p>
-                </section>
-                <div>
-            <a href="followers.php?user_id=5">Mes suiveurs</a><br>
-            <a href="subscriptions.php?user_id=5">Mes abonnements</a>      
-            </div>
-            </aside>
-            <main>
-                <?php
-                /**
-                 * Etape 3: récupérer tous les messages de l'utilisatrice
-                 */
-                $laQuestionEnSql = "
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Mur</title>
+    <meta name="author" content="Julien Falconnet">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <?php include('header.php'); ?>
+    <div id="wrapper">
+        <?php
+        /**
+         * Etape 1: Le mur concerne un utilisateur en particulier
+         * La première étape est donc de trouver quel est l'id de l'utilisateur
+         * Celui ci est indiqué en parametre GET de la page sous la forme user_id=...
+         * Documentation : https://www.php.net/manual/fr/reserved.variables.get.php
+         * ... mais en résumé c'est une manière de passer des informations à la page en ajoutant des choses dans l'url
+         */
+
+        $userId = intval($_GET['user_id']);
+        ?>
+        <?php
+        /**
+         * Etape 2: se connecter à la base de donnée
+         */
+        include('connexionbdd.php');
+        ?>
+
+        <aside>
+            <?php
+            /**
+             * Etape 3: récupérer le nom de l'utilisateur
+             */
+            $laQuestionEnSql = "SELECT * FROM users WHERE id= '$userId' ";
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            $user = $lesInformations->fetch_assoc();
+            //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
+            //echo "<pre>" . print_r($user, 1) . "</pre>";
+            ?>
+            <img src="user.jpg" alt="Portrait de l'utilisatrice" />
+            <section>
+                <h3>Présentation</h3>
+                <p>Sur cette page vous trouverez tous les message de l'utilisatrice : <?php echo $user['alias']; ?>
+
+                </p>
+            </section>
+            <div>
+                <a href="followers.php?user_id=5">Abonnés</a> - 
+                <a href="subscriptions.php?user_id=5">Abonnements</a>
+            </div><br>
+
+<div>
+            <form action="wall.php" method="post">
+                <input type='hidden' name='???' value='achanger'>
+               
+                    <label for='message'>Message</label><br>
+                    <textarea name='message'></textarea><br>
+            
+                <input type='submit'>
+            </form>
+</div>
+        </aside>
+        <main>
+            <?php
+            /**
+             * Etape 3: récupérer tous les messages de l'utilisatrice
+             */
+            $laQuestionEnSql = "
                     SELECT posts.content, posts.user_id, posts.created, users.alias as author_name, 
                     COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist,
                     GROUP_CONCAT(DISTINCT tags.id) AS tagid
@@ -71,59 +83,58 @@
                     GROUP BY posts.id
                     ORDER BY posts.created DESC  
                     ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                if ( ! $lesInformations)
-                {
-                    echo("Échec de la requete : " . $mysqli->error);
-                }
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            if (!$lesInformations) {
+                echo ("Échec de la requete : " . $mysqli->error);
+            }
 
-                /**
-                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
-                 */
-                while ($post = $lesInformations->fetch_assoc())
-                {
+            /**
+             * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
+             */
+            while ($post = $lesInformations->fetch_assoc()) {
 
-                    //echo "<pre>" . print_r($post, 1) . "</pre>";
-                    ?>                
-                    <article>
-                        <h3>
-                            <time datetime='2020-02-01 11:12:13' ><?php echo $post['created']?></time>
-                        </h3>
-                        <address><a href="wall.php?user_id=<?php echo $post['user_id'] ?>"><?php echo $post['author_name']?></a></address>
-                        <div>
-                            <p><?php echo $post['content']?></p>
-                        </div>                                            
-                        <footer>
-                            <small>♥ <?php echo $post['like_number']?></small>
-                            <?php 
+                //echo "<pre>" . print_r($post, 1) . "</pre>";
+            ?>
+                <article>
+                    <h3>
+                        <time datetime='2020-02-01 11:12:13'><?php echo $post['created'] ?></time>
+                    </h3>
+                    <address><a href="wall.php?user_id=<?php echo $post['user_id'] ?>"><?php echo $post['author_name'] ?></a></address>
+                    <div>
+                        <p><?php echo $post['content'] ?></p>
+                    </div>
+                    <footer>
+                        <small>♥ <?php echo $post['like_number'] ?></small>
+                        <?php
 
                         //TAGS 
-                        
-                        if(empty($post['taglist'])){
-                            echo "<br>"; 
+
+                        if (empty($post['taglist'])) {
+                            echo "<br>";
                         } else {
-                        $arrayTags = explode(',', $post['taglist']);
-        
-                           foreach($arrayTags as $tags) {
-                            $result = $mysqli->query("
+                            $arrayTags = explode(',', $post['taglist']);
+
+                            foreach ($arrayTags as $tags) {
+                                $result = $mysqli->query("
                             SELECT id FROM tags WHERE label='$tags'
-                            ");  
+                            ");
 
-                            $row = $result->fetch_array(MYSQLI_NUM);
+                                $row = $result->fetch_array(MYSQLI_NUM);
 
-                            //echo "<pre>" . print_r($row, 1) . "</pre>";
+                                //echo "<pre>" . print_r($row, 1) . "</pre>";
 
-                            echo '<a href="tags.php?tag_id='.$row[0][0].'">#' . $tags . ' </a>';
-                            } 
+                                echo '<a href="tags.php?tag_id=' . $row[0][0] . '">#' . $tags . ' </a>';
+                            }
                         }
                         ?>
-                            
-                        </footer>
-                    </article>
-                <?php } ?>
+
+                    </footer>
+                </article>
+            <?php } ?>
 
 
-            </main>
-        </div>
-    </body>
+        </main>
+    </div>
+</body>
+
 </html>
