7c709bd33dcf5958b09f2e2939413ca179227d57
Myriam-Mnnr <98812962+Myriam-Mnnr@users.noreply.github.com>
2022-06-07 10:31:38 +0200

    fixed formulaire pour poster un message

--- resoc_n1/Niveau1/wall.php
+++ resoc_n1/Niveau1/wall.php
@@ -50,12 +50,59 @@
                 </p>
             </section>
             <div>
-                <a href="followers.php?user_id=5">Abonnés</a> - 
-                <a href="subscriptions.php?user_id=5">Abonnements</a>
+                <a href="followers.php?user_id=<?php echo $userId ?>">Abonnés</a> - 
+                <a href="subscriptions.php?user_id=<?php echo $userId ?>">Abonnements</a>
             </div><br>
 
+
+
+            <!-- FORMULAIRE : POSTER UN MESSAGE SUR SON MUR --> 
+
+            <?php
+
+            // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
+            // si on recoit un champs email rempli il y a une chance que ce soit un traitement
+            $enCoursDeTraitement = isset($_POST['message']);
+                    if ($enCoursDeTraitement)
+                    {
+                        // on ne fait ce qui suit que si un formulaire a été soumis.
+                        // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
+                        // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
+                        //echo "<pre>" . print_r($_POST, 1) . "</pre>";
+                        // et complétez le code ci dessous en remplaçant les ???
+                        //$authorId = $_POST['auteur'];
+                        $postContent = $_POST['message'];
+                        $authorId = $userId; 
+
+
+                        //Etape 3 : Petite sécurité
+                        // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                        $authorId = intval($mysqli->real_escape_string($authorId));
+                        $postContent = $mysqli->real_escape_string($postContent);
+                        //Etape 4 : construction de la requete
+                        $lInstructionSql = "INSERT INTO posts "
+                                . "(id, user_id, content, created, parent_id) "
+                                . "VALUES (NULL, "
+                                . $authorId . ", "
+                                . "'" . $postContent . "', "
+                                . "NOW(), "
+                                . "NULL);"
+                                ;
+                        //echo $lInstructionSql;
+                        // Etape 5 : execution
+                        $ok = $mysqli->query($lInstructionSql);
+                        if ( ! $ok)
+                        {
+                            echo "Impossible d'ajouter le message: " . $mysqli->error;
+                        } else
+                        {
+                            echo "Message posté en tant que :" . $userId;
+                        }
+                    }
+                    ?> 
+
 <div>
-            <form action="wall.php" method="post">
+            <form action="wall.php?user_id=<?php echo $userId ?>" method="post">
                 <input type='hidden' name='???' value='achanger'>
                
                     <label for='message'>Message</label><br>
@@ -65,6 +112,11 @@
             </form>
 </div>
         </aside>
+
+
+
+
+
         <main>
             <?php
             /**
